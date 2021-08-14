d3175035d31cdc2022e7acae892534e448b1ec25
Angel Porteous <angelporteous27@gmail.com>
2021-08-14 13:28:35 +1200

    add files

---
+++ src/main/java/tut4/App.java
@@ -0,0 +1,29 @@
+package tut4;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
+
+
+/*
+public class App 
+{	
+   public int add(int a, int b) {
+	   return a + b;
+   }
+   
+   public int subtract(int a, int b) {
+	   return a + b;
+   }
+}
+*/
---
+++ src/test/java/tut4/AppTest.java
@@ -0,0 +1,24 @@
+package tut4;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    @Test
+    public void appTest()
+    {
+        assertEquals(2, 2);
+    }
+
+
+}
